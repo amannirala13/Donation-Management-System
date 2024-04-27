d9d347b6c7ea630351cf5ec1cf84636447be56e8
yukendhiran <yukendhiran4@gmail.com>
2024-04-27 21:07:52 +0530

    Adding Campaign Model

--- backend/src/database/DBModels.js
+++ backend/src/database/DBModels.js
@@ -1,5 +1,6 @@
 import {Collections} from "./const/Collections.js";
 import {UserModel} from "../models/auth/UserModel.js";
+import { CampaignModel } from "../models/campaign/CampaignModel.js";
 import mongoose from "mongoose";
 import {ReferralNetworkModel} from "../models/referral/ReferralNetworkModel.js";
 import {Model} from "mongoose";
@@ -10,9 +11,15 @@ const {model} = mongoose;
 export const UserMongoModel: Model = model(
     Collections.users,
     UserModel.getSchema());
+
 export const ReferralNetworkMongoModel: Model = model(
     Collections.referralNetworks,
     ReferralNetworkModel.getSchema());
+
+export const CampaignMongoModel: Model = model(
+    Collections.campaigns,
+    CampaignModel.getSchema()); 
+
 export const AdminMongoModel: Model = model(
     Collections.admins,
     AdminModel.getSchema());
---
+++ backend/src/models/campaign/CampaignModel.js
@@ -0,0 +1,107 @@
+import { Date } from "mongoose";
+
+export class CampaignModel {
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get owner(): string {
+    return this._owner;
+  }
+
+  set owner(value: string) {
+    this._owner = value;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(value: string) {
+    this._title = value;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  set description(value: string) {
+    this._description = value;
+  }
+
+  get goalAmount(): number {
+    return this._goalAmount;
+  }
+
+  set goalAmount(value: number) {
+    this._goalAmount = value;
+  }
+
+  get currentAmount(): number {
+    return this._currentAmount;
+  }
+
+  set currentAmount(value: number) {
+    this._currentAmount = value;
+  }
+
+  get startDate(): Date {
+    return this._startDate;
+  }
+
+  set startDate(value: Date) {
+    this._startDate = value;
+  }
+
+  get endDate(): Date {
+    return this._endDate;
+  }
+
+  set endDate(value: Date) {
+    this._endDate = value;
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+  set status(value: string) {
+    this._status = value;
+  }
+
+  get donations(): DonationModel[] {
+    return this._donations;
+  }
+
+  set donations(value: DonationModel[]) {
+    this._donations = value;
+  }
+
+  constructor(
+    id: string,
+    owner: string,
+    title: string,
+    description: string,
+    goalAmount: number,
+    currentAmount: number,
+    startDate: Date,
+    endDate: Date,
+    status: string,
+    donations: DonationModel[]
+  ) {
+    this._id = id;
+    this._owner = owner;
+    this._title = title;
+    this._description = description;
+    this._goalAmount = goalAmount;
+    this._currentAmount = currentAmount;
+    this._startDate = startDate;
+    this._endDate = endDate;
+    this._status = status;
+    this._donations = donations;
+  }
+}
