c61b732a5cfd9d31e6e722e738744b0d91f3cf43
Shashank Kothari <marutikothari39@gmail.com>
2024-04-28 03:27:49 +0530

    Update DepositPage.jsx

--- client/src/pages/DashboardPage/DepositPage.jsx
+++ client/src/pages/DashboardPage/DepositPage.jsx
@@ -1,28 +1,10 @@
-import {
-    Button,
-    CloseButton,
-    Input,
-    Table,
-    TableCaption,
-    TableContainer,
-    Tbody,
-    Td,
-    Tfoot,
-    Th,
-    Thead,
-    Tr
-} from "@chakra-ui/react";
-import {BiDownArrow, BiLeftDownArrowCircle, BiSearch, BiSolidDownArrow, BiWallet} from "react-icons/bi";
 import React from "react";
-import MediaQuery from "react-responsive";
-import {AiOutlineClear, AiOutlineDown} from "react-icons/ai";
-import {CiSearch} from "react-icons/ci";
-import {MdClear} from "react-icons/md";
+import { DepositDonation } from "../../components/DepositDonation/DepositDonation";
 
 export default ({className})=>{
     return(
         <div className={'w-full h-full p-4'}>
-           Dashboard
+           <DepositDonation></DepositDonation>
         </div>
     )
 }
